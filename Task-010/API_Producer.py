0253bcdd2ff47766c65253dabace2c2efcb16d61
Ridhwaan-EIT <ridhwaan.rahman@enhanceit.us>
2020-11-17 08:55:55 -0500

    .

--- Task-010/API_Producer.py
+++ Task-010/API_Producer.py
@@ -5,7 +5,7 @@ from kafka import KafkaProducer
 import requests
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9094'])
-body= {"token":"xoxb-846711423270-1513455157508-YWMoTGGIVstWq108VD1lrtwX","include_locale":"true","channel":"G01EDRKPVK2"}
+body= {"token":"xoxb-846711423270-1513455157508-YWMoTGGIVstWq108VD1lrtwX","include_locale":"true","channel":"CQLGZMJKT"}
 x= requests.get('https://slack.com/api/groups.info',params=body)
 result=x.text
 result_list= result.splitlines()
